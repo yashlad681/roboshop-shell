d270a2b4b8bae6359d29cb0f1c5d3d7c2d1b0f74
yashlad681 <yashlad681@gmail.com>
2025-04-21 04:02:05 +0530

    Roboshop project

--- frontend.sh
+++ frontend.sh
@@ -3,3 +3,9 @@ dnf module enable nginx:1.24 -y
 dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+#Config setup yet to be added
+systemctl restart nginx
