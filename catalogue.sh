5c77dd2a55038cbb385163ed7907fbeeec211c88
yashlad681 <yashlad681@gmail.com>
2025-04-23 06:20:36 +0530

    Roboshop project

--- catalogue.sh
+++ catalogue.sh
@@ -4,13 +4,13 @@ source common.sh
 
 
 #cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 
 nodejs
 #systemctl daemon-reload
 #systemctl enable catalogue
 #systemctl start catalogue
-
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.yashdevops.online </app/db/master-data.js
 
