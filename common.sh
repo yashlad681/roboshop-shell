3c39faae68db5c563004a7f5feb585399f9f31fb
yashlad681 <yashlad681@gmail.com>
2025-04-23 06:44:20 +0530

    Roboshop project

--- common.sh
+++ common.sh
@@ -13,7 +13,7 @@ nodejs(){
   rm -rf /app
   mkdir /app
   cd /app
-  npm instal
+  npm install
   curl -L -o /tmp/${component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_name}-v3.zip
   cd /app
   unzip /tmp/${component_name}.zip
