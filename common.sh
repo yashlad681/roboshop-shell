e693ab54c721102795d55a1665d4c7bff16bc97a
yashlad681 <yashlad681@gmail.com>
2025-04-23 06:56:27 +0530

    Roboshop project

--- common.sh
+++ common.sh
@@ -22,3 +22,19 @@ nodejs(){
 }
 
 pwd=$(pwd)
+
+python() {
+  dnf install python3 gcc python3-devel -y
+  cp payment.service /etc/systemd/system/payment.service
+
+  useradd roboshop
+  rm -rf /app
+  mkdir /app
+
+  curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
+  cd /app
+  unzip /tmp/payment.zip
+
+  cd /app
+  pip3 install -r requirements.txt
+}
--- payment.sh
+++ payment.sh
@@ -1,17 +1,6 @@
-dnf install python3 gcc python3-devel -y
-cp payment.service /etc/systemd/system/payment.service
+component_name=payment
+source common.sh
 
-useradd roboshop
-rm -rf /app
-mkdir /app
+python
 
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
-cd /app
-unzip /tmp/payment.zip
-
-cd /app
-pip3 install -r requirements.txt
-
-systemctl daemon-reload
-systemctl enable payment
-systemctl start payment
+systemd_setup
