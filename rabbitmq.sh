05aa415e67bbebfab550ccad7ff4f95384789dc3
yashlad681 <yashlad681@gmail.com>
2025-04-24 05:30:02 +0530

    Roboshop project

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,10 +1,16 @@
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+if [ -z "$1" ]; then
+  echo Input password is missing
+  exit
+fi
 
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 dnf install rabbitmq-server -y
 
 
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop $1
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+
+#roboshop123
