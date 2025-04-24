9a463bab465fe942c96abc737ec1d059d799d253
yashlad681 <yashlad681@gmail.com>
2025-04-24 05:54:39 +0530

    Roboshop project

--- shipping.sh
+++ shipping.sh
@@ -1,3 +1,8 @@
+if [ -z "$1" ]; then
+  echo Input DB pass is missing
+  exit
+fi
+
 component_name=shipping
 source common.sh
 
@@ -5,8 +10,15 @@ java
 
 
 dnf install mysql -y
-mysql -h mysql-dev.yashdevops.online -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.yashdevops.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.yashdevops.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+
+for file in schema app-user master-data; do
+  mysql -h mysql-dev.yashdevops.online -uroot -p$1 < /app/db/$file.sql
+done
+
+#mysql -h mysql-dev.yashdevops.online -uroot -pRoboShop@1 < /app/db/schema.sql
+#mysql -h mysql-dev.yashdevops.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+#mysql -h mysql-dev.yashdevops.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 systemd_setup
+
+#RoboShop@1
